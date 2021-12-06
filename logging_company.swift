43859a088da72be28f944ff40a19d863ef2bc321
Ubuntu <ubuntu@ip-172-31-95-119.ec2.internal>
2021-12-06 18:14:32 +0000

    Moved constants

--- logging_company.swift
+++ logging_company.swift
@@ -7,11 +7,13 @@
   given that the mass of the logs are 20 kg/m and the length of the log is inputted by the user.
 */
 
+// declare constants
+let logWeight = 20
+let truckLimit = 1100
+
 // Ask user for the length of the log and reads it
 print("What is the length of each log (0.25 metres, 0.50 metres, and 1 metres)?")
 if let logLength = Double(readLine()!) {
-	let logWeight = 20
-        let truckLimit = 1100
 	// calculates and displays the number of logs 
 	let logCount = Int(Double(truckLimit) / (logLength * Double(logWeight)))
 	print("The number of logs of length \(logLength) m and weight \(logWeight) kg/m that can be carried is \(logCount).\n")
