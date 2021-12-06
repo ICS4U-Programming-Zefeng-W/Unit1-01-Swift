d4f19673c4e7b1df5d34e2e5db875321bcbc6394
Ubuntu <ubuntu@ip-172-31-95-119.ec2.internal>
2021-12-06 17:37:30 +0000

    Updated comments

--- logging_company.swift
+++ logging_company.swift
@@ -7,12 +7,12 @@
   given that the mass of the logs are 20 kg/m and the length of the log is inputted by the user.
 */
 
-// Prompts the user to input one of three log lengths
+// Ask user for the length of the log and reads it
 print("What is the length of each log (0.25 metres, 0.50 metres, and 1 metres)?")
-// Reads user's input and does the necessary calculations to find the number of logs that can fit
 if let logLength = Double(readLine()!) {
 	let logWeight = 20
-	let truckLimit = 1100
+        let truckLimit = 1100
+	// calculates and displays the number of logs 
 	let logCount = Int(Double(truckLimit) / (logLength * Double(logWeight)))
 	print("The number of logs of length \(logLength) m and weight \(logWeight) kg/m that can be carried is \(logCount).\n")
 }
